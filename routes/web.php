fbba4be86cb4590fcdaa8ec811c227987a650622
Shahriar Shabbir <theshahriar@yahoo.com>
2018-03-24 19:48:07 +0600

    Routes added.

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,34 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Authentication routes...
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Status module routes...
+
+Route::group(['prefix' => 'status'], function () {
+
+    Route::get('/', 'StatusController@index')->name('status_index');
+    Route::get('create', 'StatusController@create')->name('status_create');
+    Route::post('store', 'StatusController@store')->name('status_store');
+    Route::get('{id}/show', 'StatusController@show')->name('status_show');
+    Route::get('{id}/edit', 'StatusController@edit')->name('status_edit');
+    Route::post('{id}/update', 'StatusController@update')->name('status_update');
+    Route::get('{id}/destroy', 'StatusController@destroy')->name('status_destroy');
+
+});
+
+// Task module routes...
+
+Route::group(['prefix' => 'task'], function () {
+
+    Route::get('/', 'TaskController@index')->name('task_index');
+    Route::get('create', 'TaskController@create')->name('task_create');
+    Route::post('store', 'TaskController@store')->name('task_store');
+    Route::get('{id}/show', 'TaskController@show')->name('task_show');
+    Route::get('{id}/edit', 'TaskController@edit')->name('task_edit');
+    Route::post('{id}/update', 'TaskController@update')->name('task_update');
+    Route::get('{id}/destroy', 'TaskController@destroy')->name('task_destroy');
+
+});
